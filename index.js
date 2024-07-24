1a2361d94ba7a4ac8143d8015037b073e7123ff4
Asif <asifhaque0505@gmail.com>
2024-07-24 10:36:00 +0530

    added

--- index.js
+++ index.js
@@ -2,11 +2,39 @@ gsap.to(".animated-text", {
   scrollTrigger: {
     trigger: "section",
     start: "top start",
+    end:"bottom center",
     scrub: 1,
     markers: true,
     pin: true,
   },
-  y: -1000,
+  y: -100,
+  //   textContent: "ABC",
   ease: "none",
   duration: 5,
 });
+
+// gsap.to(".left", {
+//   scrollTrigger: {
+//     trigger: "section",
+//     start: "top start",
+//     scrub: 1,
+//     pin: true,
+//   },
+//   backgroundColor: "rgb(77, 124, 15)",
+//   ease: "none",
+//   duration: 5,
+// });
+
+// gsap.to(".left", {
+//   scrollTrigger: {
+//     trigger: "section",
+//     start: "top top",
+//     end: "bottom top", // Adjust the end position to your liking
+//     scrub: 1,
+//     markers: true,
+//     pin: true,
+//   },
+//   backgroundColor: "rgb(77, 124, 15)",
+//   ease: "none",
+//   duration: 5,
+// });
