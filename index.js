80e8ed47a17c2908e781ec23d0f1e3b0625fa8f7
Asif <asifhaque0505@gmail.com>
2024-07-25 08:51:49 +0530

    added styling in right

--- index.js
+++ index.js
@@ -13,23 +13,37 @@ const tl = gsap.timeline({
 tl.to(
   ".left",
   {
-    //   scrollTrigger: st,
     backgroundColor: "rgb(22 163 74)",
     ease: "none",
     duration: 5,
   },
-  0
+  5
+).to(
+  ".left",
+  {
+    backgroundColor: "rgb(23 37 84)",
+    ease: "none",
+    duration: 5,
+  },
+  15
 );
 
 tl.to(
   ".right",
   {
-    //   scrollTrigger: st,
     backgroundColor: "rgb(20 83 45)",
     ease: "none",
     duration: 5,
   },
-  0
+  5
+).to(
+  ".right",
+  {
+    backgroundColor: "rgb(30 58 138)",
+    ease: "none",
+    duration: 5,
+  },
+  15
 );
 
 tl.to(
@@ -41,13 +55,96 @@ tl.to(
     duration: 5,
   },
   0
-);
-// .to(".animated-text", {
-//   y: 0,
-//   //   textContent: "ABC",
-//   ease: "none",
-//   duration: 3,
-// });
+)
+  .to(
+    ".animated-text-2",
+    {
+      y: -100,
+      duration: 5,
+    },
+    5
+  )
+  .to(
+    ".animated-text-2",
+    {
+      y: -200,
+      duration: 5,
+    },
+    10
+  )
+  .to(
+    ".animated-text-3",
+    {
+      y: -200,
+      duration: 5,
+    },
+    15
+  );
+
+tl.to(
+  ".image-1",
+  {
+    y: 450,
+    duration: 5,
+  },
+  0
+)
+  .to(
+    ".image-1r",
+    {
+      y: -410,
+      duration: 5,
+    },
+    0
+  )
+  .to(
+    ".image-2",
+    {
+      y: -450,
+      duration: 5,
+    },
+    5
+  )
+  .to(
+    ".image-2r",
+    {
+      y: -410,
+      duration: 5,
+    },
+    5
+  )
+  .to(
+    ".image-2",
+    {
+      y: 450,
+      duration: 5,
+    },
+    10
+  )
+  .to(
+    ".image-2r",
+    {
+      y: -820,
+      duration: 5,
+    },
+    10
+  )
+  .to(
+    ".image-3",
+    {
+      y: -1000,
+      duration: 5,
+    },
+    15
+  )
+  .to(
+    ".image-3r",
+    {
+      y: -820,
+      duration: 5,
+    },
+    15
+  );
 
 // gsap.to(".left", {
 //   scrollTrigger: {
